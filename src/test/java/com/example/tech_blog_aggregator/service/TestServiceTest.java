9eea086b55958b425c91b1c7b6917eb7f12f6078
Gukin Han <gukin.dev@gmail.com>
2024-10-05 00:28:59 +0900

    test: 디스플레이 이름을 추가

--- src/test/java/com/example/tech_blog_aggregator/service/TestServiceTest.java
+++ src/test/java/com/example/tech_blog_aggregator/service/TestServiceTest.java
@@ -1,11 +1,13 @@
 package com.example.tech_blog_aggregator.service;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestServiceTest {
 
     @Test
+    @DisplayName("두 정수의 합을 반환한다")
     void sumTest() {
         int a = 3;
         int b = 5;
